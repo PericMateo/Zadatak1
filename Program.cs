f99ed1fe89f47063732466c2858b9c6b4e8d66c3
PericMateo <44578467+PericMateo@users.noreply.github.com>
2020-03-10 19:40:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /*Napisati program koji ispisuje tablicu množenja do broja 12*/
+        static void Main(string[] args)
+        {
+            int i, j, k;
+            for (i = 1; i <= 12; i++)
+            {
+                for (k = 1; k <= 12; k++)
+                {
+                    j = i * k;
+                    Console.Write(i + "x" + k +"=" + j +" ");
+                }
+                Console.WriteLine(" ");
+            }
+           
+            
+        }
+    }
+}
